56fc14241b0aac3d92fd43505ff3e456c701a6b4
G Venkata Chalapathi <thegvc971@gmail.com>
2025-08-02 18:43:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// TODO APP
+
+let btn = document.querySelector("button");
+let input = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+    let item = document.createElement("li");
+    item.innerText = input.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+
+    if(input.value != ""){
+        ul.appendChild(item);
+    }
+    input.value = "";
+});
+ul.addEventListener("click", function (event) {
+    if(event.target.nodeName == "BUTTON"){
+        let par = event.target.parentElement;
+        par.remove();
+    } 
+} );
